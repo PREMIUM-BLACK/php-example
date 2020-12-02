e74c4831c2ddd73bba3f3269fdb3a0e6975b2542
Florian Dahn <florian.dahn@fdsoftware.de>
2020-12-03 00:56:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -31,6 +31,18 @@ return;
 	$request->IPN = "NO_IPN";
 	$request->BlockAddress ="true";
 	
+	//Add custom User Id for accounting
+	$request->CustomUserId = "1234";
+
+	//Add custom Order Id for accounting
+	$request->CustomOrderId = "24";
+
+	//Add customer mail address for accounting
+	$request->CustomerMail = "me@mail.com";
+
+	//Anything else you want to link with this transaction
+	$request->CustomData = "186ebff2-b319-49ed-a18f-dcbb27c9e9dd";
+	
 	$response = $api->CreateTransaction($publicKey, $request);
 	
 	if(!$api->checkHash($response))
